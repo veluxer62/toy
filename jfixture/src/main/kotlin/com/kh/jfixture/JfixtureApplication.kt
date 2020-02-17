71130d123f6d6913ecbd3c47bed2da94832acba2
남경호 <anytimedebug@tripstore.kr>
2020-02-17 16:52:56 +0900

    Initialize jfixture test module

---
+++ jfixture/src/main/kotlin/com/kh/jfixture/JfixtureApplication.kt
@@ -0,0 +1,11 @@
+package com.kh.jfixture
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JfixtureApplication
+
+fun main(args: Array<String>) {
+    runApplication<JfixtureApplication>(*args)
+}
