3ca9d080d3d5d11041f715f22b501148b86085a5
남경호 <anytimedebug@tripstore.kr>
2020-02-17 16:52:56 +0900

    Initialize jfixture test module

---
+++ jfixture/src/test/kotlin/com/kh/jfixture/JfixtureApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kh.jfixture
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JfixtureApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
