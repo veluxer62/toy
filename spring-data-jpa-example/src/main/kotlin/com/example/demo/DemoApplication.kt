586d126ff4449fae5dd85c1fb5b6ed3c27b4be36
남경호 <veluxer62@gmail.com>
2020-03-28 19:15:19 +0900

    Create spring data jpa example

---
+++ spring-data-jpa-example/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -0,0 +1,13 @@
+package com.example.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+
+@SpringBootApplication
+@EnableJpaRepositories
+class DemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoApplication>(*args)
+}
