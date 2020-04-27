93ec0efd9d334a785174335387a06066b81af953
남경호 <veluxer62@gmail.com>
2020-04-28 00:54:04 +0900

    Create algorithm study code

---
+++ algorithm/src/part01/chapter01/section01/Code1_2.kt
@@ -0,0 +1,15 @@
+package part01.chapter01.section01
+
+fun main() {
+    println("Hello, World!")
+    println("Merry Christmas")
+    println("Happy New Year")
+
+    println("Hello, World!")
+    println("Merry Christmas")
+    println("Happy New Year")
+
+    println("Hello, World!")
+    println("Merry Christmas")
+    println("Happy New Year")
+}
---
+++ algorithm/src/part01/chapter01/section01/Code1_3.kt
@@ -0,0 +1,9 @@
+package part01.chapter01.section01
+
+fun main() {
+    for (i in 0..2) {
+        println("Hello, World!")
+        println("Merry Christmas")
+        println("Happy New Year")
+    }
+}
